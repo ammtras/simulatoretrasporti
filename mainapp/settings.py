5f8abfbb9fe890af6090de80fab5feec82ab0a16
ammtras <amm.pell@gmail.com>
2026-06-16 02:18:21 +0200

    Configuro path database tramite variabile ambiente

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -74,9 +74,12 @@ WSGI_APPLICATION = 'mainapp.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.environ.get(
+            "SQLITE_PATH",
+            BASE_DIR / "db.sqlite3"
+        ),
     }
 }
 
